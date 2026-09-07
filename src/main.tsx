a5b5b2d7ee3aa0e3520dd18249ee77f8d5b8453c
Zehan <zehandesigns@gmail.com>
2026-09-07 15:46:19 +0800

    ci: add GitHub Pages workflow for the testing repo

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "lenis/dist/lenis.css";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <App />
   </BrowserRouter>
 );
