c2ce1b360186b070c60e0b62dba679bb3aa89184
Zehan <zehandesigns@gmail.com>
2026-09-07 15:37:30 +0800

    Revert GitHub Pages testing setup
    Dropping the separate Pages deployment in favor of a PR into main.

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "lenis/dist/lenis.css";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename={import.meta.env.BASE_URL}>
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
